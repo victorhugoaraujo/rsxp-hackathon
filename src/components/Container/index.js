d813f3e5a5e85dd4a9c4af07843590741a699fe4
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2019-11-26 18:23:49 -0300

    First commit, layout

---
+++ src/components/Container/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles.css';
+import rocket from '../../images/rocket.svg';
+import moon from '../../images/moon.svg';
+// import logo from './assets/logo.svg';
+
+export default function Container(){
+  return (
+    <div className='container'>
+      <div className='box-code'>
+        <div className='sdjak'>
+          <p>Instruções de codigo</p>
+        </div>
+        <input></input>
+      </div>
+      
+      <div className='box-result'>
+        <div className='rocket'>
+          <img src={rocket} alt=""/>
+        </div>
+
+        <div className="moon">
+          <img src={moon} alt=""/>
+        </div>
+      </div>
+    </div>
+  )
+}
